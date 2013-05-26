74066f6e8da90aed16aa01c12c6d0fb367d8d46c
Pan Ziyue <random.rrr3@gmail.com>
2013-05-26 21:29:42 +0800

    Initial Commit

---
+++ SST Announcements/SSTAnnounceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SSTAnnounceAppDelegate.h
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 26/5/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSTAnnounceAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SST Announcements/SSTAnnounceViewController.m
@@ -0,0 +1,29 @@
+//
+//  SSTAnnounceViewController.m
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 26/5/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import "SSTAnnounceViewController.h"
+
+@interface SSTAnnounceViewController ()
+
+@end
+
+@implementation SSTAnnounceViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
