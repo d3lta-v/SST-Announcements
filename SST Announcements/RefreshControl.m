e9ba825c3fd24e70798fcac46705c30bc7b4d16d
Pan Ziyue <random.rrr3@gmail.com>
2013-07-07 22:29:40 +0800

    Removed push files

---
+++ SST Announcements/RefreshControl.m
@@ -0,0 +1,31 @@
+//
+//  RefreshControl.m
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 7/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import "RefreshControl.h"
+
+@implementation RefreshControl
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
