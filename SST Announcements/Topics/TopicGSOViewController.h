ccc4b86f5fb5ab8cc7b5f9037f5e0856d743e9a4
Pan Ziyue <random.rrr3@gmail.com>
2013-07-10 22:55:06 +0800

    added 1 topic, added alphabetical sections for the list of categories

---
+++ SST Announcements/Topics/TopicGSOViewController.h
@@ -0,0 +1,15 @@
+//
+//  TopicGSOViewController.h
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 10/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicGSOViewController : UITableViewController <NSXMLParserDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
