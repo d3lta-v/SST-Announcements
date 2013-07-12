457fe19a3d998e2ec6a44aea6115a16a77bc2b1d
Pan Ziyue <random.rrr3@gmail.com>
2013-07-13 00:30:01 +0800

    added a category

---
+++ SST Announcements/Topics/TopicOrientationViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicOrientationViewController.h
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 13/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicOrientationViewController : UITableViewController <NSXMLParserDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@end
