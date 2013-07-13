eb4add24a9607541ed8bbfe41ea2deffe01561d5
Pan Ziyue <random.rrr3@gmail.com>
2013-07-13 21:25:42 +0800

    added 3 more categories, added non-functional feedback button

---
+++ SST Announcements/Topics/TopicAffairsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicAffairsViewController.h
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 13/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicAffairsViewController : UITableViewController <NSXMLParserDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ SST Announcements/Topics/TopicReopenViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopicReopenViewController.h
+//  SST Announcements
+//
+//  Created by Pan Ziyue on 13/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopicReopenViewController : UITableViewController <NSXMLParserDelegate, UITableViewDataSource, UITableViewDelegate>
+
+@end
